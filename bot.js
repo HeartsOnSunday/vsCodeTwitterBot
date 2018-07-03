9e5dc1567259f083586d7c2308b70d704992e2db
HeartsOnSunday <leahgdg@hotmail.com>
2018-07-02 20:40:31 -0400

    keys hidden in .gitignore

--- bot.js
+++ bot.js
@@ -1,1 +1,13 @@
 console.log('Its Alive');
+
+var Twit = require('twit');
+
+//new twit object
+var T = new Twit({
+    consumer_key:         'iR3gK5exixXFf3ihRLJLZxSkZ',
+    consumer_secret:      'YMpJJcjXFtj3U9XnZ2e6JI3vJLW1ZDIrUXyw2lduY4OwzJbfhS',
+    access_token:         '...',
+    access_token_secret:  '...',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL:            true,     // optional - requires SSL certificates to be valid.
+  })
