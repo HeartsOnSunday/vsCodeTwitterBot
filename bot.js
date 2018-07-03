2126ae4a66172488919ca64b82cfd67de42a55d4
HeartsOnSunday <leahgdg@hotmail.com>
2018-07-03 13:37:31 -0400

    required express

--- bot.js
+++ bot.js
@@ -1,11 +1,14 @@
 console.log('Its Alive');
 
 var Twit = require('twit');
+var express = require('express');
+    app = express(),
+require('dotenv').load();
 
 //new twit object
 var T = new Twit({
-    consumer_key:         KEY,
-    consumer_secret:      SECRET,
+    consumer_key:         process.env.KEY,
+    consumer_secret:      process.env.SECRET,
     access_token:         '...',
     access_token_secret:  '...',
     timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
