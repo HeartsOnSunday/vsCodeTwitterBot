5db45164d317eac42e55b63ec682703cabb381ee
HeartsOnSunday <leahgdg@hotmail.com>
2018-07-06 12:11:39 -0400

    new .config

--- bot.js
+++ bot.js
@@ -15,15 +15,15 @@ var T = new Twit(config);
 console.log(T);
 
 
-//new twit object
-//var T = new Twit({
+// new twit object
+// var T = new Twit({
 //     consumer_key:         process.env.KEY,
 //     consumer_secret:      process.env.SECRET,
 //     access_token:         process.env.TOKEN,
 //     access_token_secret:  process.env.TOKEN_SECRET,
 //     timeout_ms:           60*1000,   // optional HTTP request timeout to apply to all requests.
 //    strictSSL:            true,     // optional - requires SSL certificates to be valid.
- // });
+//  });
 
   //console.log(T);
   //console.log(T.process.env.TOKEN_SECRET);
