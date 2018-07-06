b909531cead0cacc0799c509ccdca1590d529f2d
HeartsOnSunday <leahgdg@hotmail.com>
2018-07-06 13:35:08 -0400

    fixed conflicts

--- config.js
+++ config.js
@@ -7,11 +7,9 @@
 
 module.exports = {
     consumer_key: 'iR3gK5exixXFf3ihRLJLZxSkZ',
-
     consumer_secret: 'YMpJJcjXFtj3U9XnZ2e6JI3vJLW1ZDIrUXyw2lduY4OwzJbfhS',
-
     access_token: '915409058378731521-5P4HDOOpXPjm3ZYHX7XdAD02h7ixs94',
-
-    access_token_secret: 'UVnBu1DeUY13xLE8MYeja0JErrC9r2NBntzW6q3YnIvgF'
-
+    access_token_secret: 'UVnBu1DeUY13xLE8MYeja0JErrC9r2NBntzW6q3YnIvgF',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL:            true,     // optional - requires SSL certificates to be valid.
 }
