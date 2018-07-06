8f77b2584792b6871837ae64c54c067328ef9dba
HeartsOnSunday <leahgdg@hotmail.com>
2018-07-05 21:45:10 -0400

    mst fix keys

--- bot.js
+++ bot.js
@@ -1,22 +1,32 @@
 console.log('Its Alive');
 
 var Twit = require('twit');
+console.log('twit has been required');
+
 var express = require('express');
-    app = express(),
+   app = express(),
 require('dotenv').load();
+console.log('express has started');
+
+var config = require('./config');
+//console.log(config);
+
+var T = new Twit(config); 
+console.log(T);
+
 
 //new twit object
-var T = new Twit({
-    consumer_key:         process.env.KEY,
-    consumer_secret:      process.env.SECRET,
-    access_token:         process.env.TOKEN,
-    access_token_secret:  process.env.TOKEN_SECRET,
-    //timeout_ms:           60*1000,   // optional HTTP request timeout to apply to all requests.
-   // strictSSL:            true,     // optional - requires SSL certificates to be valid.
-  })
+//var T = new Twit({
+//     consumer_key:         process.env.KEY,
+//     consumer_secret:      process.env.SECRET,
+//     access_token:         process.env.TOKEN,
+//     access_token_secret:  process.env.TOKEN_SECRET,
+//     timeout_ms:           60*1000,   // optional HTTP request timeout to apply to all requests.
+//    strictSSL:            true,     // optional - requires SSL certificates to be valid.
+ // });
 
   //console.log(T);
-  console.log(T.process.env.TOKEN_SECRET);
+  //console.log(T.process.env.TOKEN_SECRET);
 
 
 
