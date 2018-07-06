0fafbcc681aea51a6f002822f469683b4dbcae6d
HeartsOnSunday <leahgdg@hotmail.com>
2018-07-06 13:19:51 -0400

    added config.js

--- config.js
+++ config.js
@@ -1,6 +1,17 @@
-export const consumer_key = process.env.KEY;
-export const consumer_secret = process.env.SECRET;
-export const access_token = process.env.TOKEN;
-export const access_token_secret = process.env.TOKEN_SECRET;
-export const timeout_ms = 60 * 1000;
-export const strictSSL = true;
+// export const consumer_key = process.env.KEY;
+// export const consumer_secret = process.env.SECRET;
+// export const access_token = process.env.TOKEN;
+// export const access_token_secret = process.env.TOKEN_SECRET;
+// export const timeout_ms = 60 * 1000;
+// export const strictSSL = true;
+
+module.exports = {
+    consumer_key: 'iR3gK5exixXFf3ihRLJLZxSkZ',
+
+    consumer_secret: 'YMpJJcjXFtj3U9XnZ2e6JI3vJLW1ZDIrUXyw2lduY4OwzJbfhS',
+
+    access_token: '915409058378731521-5P4HDOOpXPjm3ZYHX7XdAD02h7ixs94',
+
+    access_token_secret: 'UVnBu1DeUY13xLE8MYeja0JErrC9r2NBntzW6q3YnIvgF'
+
+}
