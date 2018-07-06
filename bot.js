1097716efd389a8b2bf10a5ee2fbc301cb6e30df
HeartsOnSunday <leahgdg@hotmail.com>
2018-07-05 21:51:01 -0400

    this is broken

--- bot.js
+++ bot.js
@@ -4,12 +4,12 @@ var Twit = require('twit');
 
 //new twit object
 var T = new Twit({
-    consumer_key:         KEY,
-    consumer_secret:      SECRET,
-    access_token:         '...',
-    access_token_secret:  '...',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL:            true,     // optional - requires SSL certificates to be valid.
+    // consumer_key:         KEY,
+    // consumer_secret:      SECRET,
+    // access_token:         '...',
+    // access_token_secret:  '...',
+    // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+    // strictSSL:            true,     // optional - requires SSL certificates to be valid.
   })
 
 
---
+++ config.js
@@ -0,0 +1,6 @@
+export const consumer_key = process.env.KEY;
+export const consumer_secret = process.env.SECRET;
+export const access_token = process.env.TOKEN;
+export const access_token_secret = process.env.TOKEN_SECRET;
+export const timeout_ms = 60 * 1000;
+export const strictSSL = true;
