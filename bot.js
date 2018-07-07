b2f40bc9fb9ddbf100c08559b8fe2cefa5765c47
HeartsOnSunday <leahgdg@hotmail.com>
2018-07-07 08:32:40 -0400

    retrieves reTweets

--- bot.js
+++ bot.js
@@ -9,6 +9,26 @@ console.log(config);
 var T = new Twit(config);
 console.log(T);
 
+//ACTIONS
+//get() search by #, location or users
+//post() Tweet!!!
+//steam() streaming with the twitter api and assign certain events
+    //if someone mentions you, you can follow back.
+var params = {
+    q: 'cybersecurity',
+    count: 5,
+    lang: 'en'
+}
+
+T.get('search/tweets', params, gotData);
+
+function gotData(err, data, response) {
+    var tweets = data.statuses;
+    for (var i = 0; i < tweets.length; i++){
+        console.log(i + " : " + tweets[i].text);
+    }
+}
+
 
 // var express = require('express');
 //    app = express(),
