7f960cb566c38cfbf6587ed9d7bb40f3dcdbb5fc
HeartsOnSunday <leahgdg@hotmail.com>
2018-07-09 14:58:38 -0400

    removed exraneous credentials

--- config.js
+++ config.js
@@ -1,10 +1,3 @@
-// export const consumer_key = process.env.KEY;
-// export const consumer_secret = process.env.SECRET;
-// export const access_token = process.env.TOKEN;
-// export const access_token_secret = process.env.TOKEN_SECRET;
-// export const timeout_ms = 60 * 1000;
-// export const strictSSL = true;
-
 module.exports = {
     consumer_key: 'iR3gK5exixXFf3ihRLJLZxSkZ',
     consumer_secret: 'YMpJJcjXFtj3U9XnZ2e6JI3vJLW1ZDIrUXyw2lduY4OwzJbfhS',
